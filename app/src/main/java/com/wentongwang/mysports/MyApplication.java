af8165595fc7e83b8f200abf02d8ee34ecf1f871
Danchao <gdc0411@gmail.com>
2017-04-04 13:11:04 +0200

    change UI news page and add calligraphy lib to use 3rd fonts

--- app/src/main/java/com/wentongwang/mysports/MyApplication.java
+++ app/src/main/java/com/wentongwang/mysports/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.wentongwang.mysports.model.bussiness.VolleyQueueManager;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by Wentong WANG on 2016/9/16.
  */
@@ -12,5 +14,11 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         VolleyQueueManager.init(this);
+        // initalize Calligraphy
+        CalligraphyConfig.initDefault(
+                new CalligraphyConfig.Builder()
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
+        );
     }
 }
